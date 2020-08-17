2e59321cf9c5229c550d93297b3e149831e30e13
aonurakman <ahmetonurakman@gmail.com>
2020-08-17 17:49:32 +0300

    ...

--- Phone/phoneAppManager.swift
+++ Phone/phoneAppManager.swift
@@ -10,15 +10,15 @@ import UIKit
 
 typealias ContainsTableView = UITableViewDelegate & UITableViewDataSource
 
+protocol NewContactDelegate {
+    func didNewContactAdded()
+}
+
 enum TagDictionary: Int {
     case basicInfo = 100, ringtoneButton = 101, textToneButton = 102, notesStack = 103, phoneStack = 104, emailStack = 105, urlStack = 106, addressStack = 107, bdayStack = 108, datesStack = 109, relatedStack = 110, socialStack = 111, instantStack = 112
     case fieldToCopy = 4, hidden = 3, fieldToRead = 2, actionButton = 1, regular = 0
 }
 
 struct PhoneAppManager {
-    
-    
-    
-    
-    
+
 }
